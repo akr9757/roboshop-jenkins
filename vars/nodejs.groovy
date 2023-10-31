1be63f155e65aecb4a682b4353867a358548f92d
akr9757 <anilkumareddy9757@gmail.com>
2023-10-31 12:25:23 +0530

    terraform

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -17,7 +17,7 @@ def call() {
             stage('Code Quality') {
                 steps {
                     sh 'ls -l'
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.46.190:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.46.190:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                 }
             }
 
