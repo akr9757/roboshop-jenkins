edf9c76b51f9f83bb1834ee1758fde22cb17cbbe
akr9757 <anilkumareddy9757@gmail.com>
2023-11-05 10:45:05 +0530

    terraform

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -60,7 +60,7 @@ def call() {
                 steps {
                     sh 'mvn package ; cp target/${component}-1.0.jar ${component}.jar'
                     sh 'echo $TAG_NAME >VERSION'
-                    sh 'if [ -n "${schema_dir}" ]; then aws ssm put-parameter --name "${component}.schema.checksum" --type "String" --value "$(md5sum /app/schema/*.sql | awk "{print \\$1}" )"  --overwrite; fi'
+                    sh 'if [ -n "${schema_dir}" ]; then aws ssm put-parameter --name "${component}.schema.checksum" --type "String" --value "$(md5sum schema/*.sql | awk "{print \\$1}" )"  --overwrite; fi'
                     sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schema_dir}'
                     sh 'curl -f -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.46.188:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
